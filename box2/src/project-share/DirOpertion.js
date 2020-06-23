495365dcc1484bfdc9d0972859cfcd518403ef62
YLF <1463020554@qq.com>
2020-06-23 20:35:47 +0800

    update copyDirList先删除再复制

--- box2/src/project-share/DirOpertion.js
+++ box2/src/project-share/DirOpertion.js
@@ -2,36 +2,40 @@ const fs = require( 'fs' );
 
 /*
  * 复制目录、子目录，及其中的文件
- * @param src {String} 要复制的目录
- * @param dist {String} 复制到目标目录
+ * @param source {String} 要复制的目录
+ * @param target {String} 复制到目标目录
+ * @param callback {Function} 报错时执行的函数
  */
-function copyDir(src, dist, callback=console.error) {
-    fs.access(dist, function(err){
+function copyDir(source, target, callback=console.error) {
+    fs.access(target, function(err){
         if(err){
             // 目录不存在时创建目录
-            fs.mkdirSync(dist);
+            fs.mkdirSync(target);
         }
-        _copy(null, src, dist);
+        _copy(null, source, target);
     });
 
-    function _copy(err, src, dist) {
+    function _copy(err, source, target) {
         if(err) return callback(err);
-        fs.readdir(src, function(err, paths) {
+        fs.readdir(source, function(err, paths) {
             if(err) return callback(err);
             paths.forEach(function(path) {
-                const _src = ''.concat(src,'/',path);
-                const _dist = ''.concat(dist,'/',path);
-                fs.stat(_src, function(err, stat) {
+                const _source = ''.concat(source,'/',path);
+                const _target = ''.concat(target,'/',path);
+                fs.stat(_source, function(err, stat) {
                     if(err) return callback(err);
-                    if(stat.isFile()) return fs.writeFileSync(_dist, fs.readFileSync(_src));
-                    if(stat.isDirectory()) return copyDir(_src, _dist, callback);
+                    if(stat.isFile()) return fs.writeFileSync(_target, fs.readFileSync(_source));
+                    if(stat.isDirectory()) return copyDir(_source, _target, callback);
                 })
             })
         })
     }
 }
 
-
+/*
+ * 删除指定目录或文件
+ * @param path {String} 要删除的路径
+ */
 function delDir(path) {
     let files = [];
     if( fs.existsSync(path) ) {
@@ -48,11 +52,17 @@ function delDir(path) {
     }
 }
 
+/*
+ * 复制源目录至多个指定目录下【先删除再复制】
+ * @param source {String} 源目录
+ * @param targetList {Array} 指定目录数组
+ * @param callback {Function} 报错时执行的函数
+ */
 function copyDirList(source,targetList,callback){
     if(!Array.isArray(targetList)) console.log('targetList必须是数组');
     targetList.forEach(target=>{
+        delDir(target);
         copyDir(source,target,callback);
-        // delDir(target);
     });
     console.log(`已成功复制到所有指定目录下！`);
 }
